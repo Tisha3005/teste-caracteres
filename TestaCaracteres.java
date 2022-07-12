9188505202c87d5be2e8d12bbdf50dc68a3c5a32
Tisha3005 <ribeiro_thais@hotmail.com>
2022-07-12 11:42:21 -0300

    teste-caracteres
    usando a tabela ASCII

---
+++ TestaCaracteres.java
@@ -0,0 +1,24 @@
+
+//public class TestaCaracteres {
+
+//public static void main(String[] args) {
+
+public class TestaCaracteres {
+	public static void main(String[] args) {
+		// char letra_J = 74;
+		// char letra_P = 80;
+		// System.out.printf("%c %c\n",letra_J, letra_P);
+
+		int numero_J = 74;
+		char letra_J = (char) numero_J;
+		char letra_P = 80;
+		char letra_i = 'i';
+		System.out.printf("%c%c%c%c\n", letra_J, 97, 118, 97);
+		System.out.println("Usando a Tabela ASCII");
+		System.out.printf("84, 104, 97, 105, 115 = T H A I S");
+		
+
+		// char letra = 'a';
+	}
+
+}
